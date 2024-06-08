1a6fad8f86948199ff957cead034c80add085a3e
Gerg-L <GregLeyda@proton.me>
2024-06-08 11:32:02 -0400

    GHA

--- flake.nix
+++ flake.nix
@@ -3,35 +3,47 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, ... }:
+  outputs =
+    { self, nixpkgs, ... }:
     let
       inherit (nixpkgs) lib;
 
-      testSystems = [ "x86_64-linux" "aarch64-linux" ];
-      systems = testSystems ++ [ "x86_64-darwin" "aarch64-darwin" ];
+      testSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      systems = testSystems ++ [
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-      mkPackages = pkgs:
+      mkPackages =
+        pkgs:
         let
-          nix-index-database = pkgs.callPackage ./database.nix { };
+          nix-index-database =
+            let
+              generated = import ./generated.nix;
+            in
+            pkgs.fetchurl {
+              url = generated.url + pkgs.stdenv.system;
+              hash = generated.hashes.${pkgs.stdenv.system};
+              passthru.unsafeDiscardReferences.out = true;
+            };
         in
         {
           inherit nix-index-database;
 
-          nix-index-with-db =
-            pkgs.callPackage ./nix-index-wrapper.nix {
-              inherit nix-index-database;
-            };
-          comma-with-db =
-            pkgs.callPackage ./comma-wrapper.nix {
-              inherit nix-index-database;
-            };
+          nix-index-with-db = pkgs.callPackage ./nix-index-wrapper.nix { inherit nix-index-database; };
+          comma-with-db = pkgs.callPackage ./comma-wrapper.nix { inherit nix-index-database; };
         };
     in
     {
-      packages = lib.genAttrs systems (system:
+      packages = lib.genAttrs systems (
+        system:
         {
           default = self.packages.${system}.nix-index-with-db;
-        } // mkPackages nixpkgs.legacyPackages.${system}
+        }
+        // mkPackages nixpkgs.legacyPackages.${system}
       );
 
       overlays.nix-index = final: prev: mkPackages final;
@@ -42,7 +54,8 @@
 
       nixosModules.nix-index = import ./nixos-module.nix self;
 
-      checks = lib.genAttrs testSystems (system:
+      checks = lib.genAttrs testSystems (
+        system:
         import ./tests.nix {
           inherit system nixpkgs;
           nixIndexModule = self.nixosModules.nix-index;
